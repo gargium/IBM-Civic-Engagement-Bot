e2803562311223f26eff706d1a1a4f6ffa6ce129
andyyu <andyhsyu@gmail.com>
2017-06-09 00:34:01 -0700

    Remove extraneous location updates

--- selenium_test.py
+++ selenium_test.py
@@ -79,13 +79,11 @@ class ChatBotTestCase(unittest.TestCase):
 
     def test_4_representatives(self):
         print "Testing representatives."
-        self.getBotResponse("443 Midvale Ave, Los Angeles, CA 90024")
         response = self.getBotResponse("Who are my representatives?")
         self.assertTrue(response.startswith("Your representatives are"))
 
     def test_5_contact_representatives(self):
         print "Testing representatives contacts."
-        self.getBotResponse("443 Midvale Ave, Los Angeles, CA 90024")
         response = self.getBotResponse("Contact my reps.")
         self.assertTrue("Website" in response)
 
